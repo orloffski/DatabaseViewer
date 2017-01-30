a7a1fab147f4d516bb709ee86bfe9dc22fe3fd5e
Evgen Orlovsky <orloffski@gmail.com>
2017-01-30 12:27:59 +0300

    Rev 1.007.6 MS SQL types changed

--- app/src/main/java/by/madcat/development/databaseviewer/Utils/SqlTypes.java
+++ app/src/main/java/by/madcat/development/databaseviewer/Utils/SqlTypes.java
@@ -1,20 +1,31 @@
 package by.madcat.development.databaseviewer.Utils;
 
 public enum SqlTypes {
+    // data
+    MONEY("money"),
+
+    // numbers
+    TINYINT("tinyint"),
     SMALLINT("smallint"),
     INT("int"),
+    BIGINT("bigint"),
     REAL("real"),
-    MONEY("money"),
-    TINYINT("tinyint"),
+
     BIT("bit"),
+    FLOAT("float"),
     DECIMAL("decimal"),
+
+    // text
     CHAR("char"),
     VARCHAR("varchar"),
     TEXT("text"),
     IMAGE("image"),
+
+    // date-time
     DATE("date"),
     TIME("time"),
-    DATETIME("datetime");
+    DATETIME("datetime"),
+    TIMESTAMP("timestamp");
 
     private String id;
 
